3e0ba3870a5d97dfd4e9eb564371427067893bc1
Amaury Zarzelli <amaury.zarzelli@gmail.com>
2024-10-16 10:22:57 +0200

    change scale of scores, losing point strating from 10m instead of 1m

--- js/scoring.js
+++ js/scoring.js
@@ -4,7 +4,7 @@
 * @returns {int} score associated with the distance
 */
 function distanceToScore(dist) {
-  return Math.round(Math.min(5000, Math.max(0, 5000 - Math.log10(dist) * 2500/3)))
+  return Math.round(Math.min(1000, Math.max(0, 1000 - Math.log10(dist - 9) * 500/3)))
 }
 
 export { distanceToScore }
