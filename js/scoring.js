73e74e5bdeac8ad8b776d00a81460f07763f60dc
Amaury Zarzelli <amaury.zarzelli@ign.fr>
2024-10-16 10:30:11 +0200

    Update scoring.js

--- js/scoring.js
+++ js/scoring.js
@@ -4,7 +4,7 @@
 * @returns {int} score associated with the distance
 */
 function distanceToScore(dist) {
-  return Math.round(Math.min(1000, Math.max(0, 1000 - Math.log10(max(1, dist - 9)) * 500/3)))
+  return Math.round(Math.min(1000, Math.max(0, 1000 - Math.log10(Math.max(1, dist - 9)) * 500/3)))
 }
 
 export { distanceToScore }
